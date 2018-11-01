4ecffb8fc0ce4624b5c2efc5edf877e4c25c3be6
Sheng Liu <sheng1302@gmail.com>
2018-11-01 16:09:21 -0400

    heroku settings

--- models/index.js
+++ models/index.js
@@ -9,8 +9,11 @@ const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-    sequelize = new Sequelize(process.env[config.use_env_variable], config);
+//config.use_env_variable
+//
+if (process.env.HEROKU_POSTGRESQL_BRONZE_URL) {
+    //sequelize = new Sequelize(process.env[config.use_env_variable], config);
+    sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_BRONZE_URL, config);
 } else {
     sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
