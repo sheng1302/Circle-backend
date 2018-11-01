f7e9063baf1379f976d554803a3dfe46a096fb7e
Sheng Liu <sheng1302@gmail.com>
2018-11-01 16:40:12 -0400

    change production

--- models/index.js
+++ models/index.js
@@ -4,16 +4,16 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || 'production';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
-//config.use_env_variable
 //
-if (process.env.HEROKU_POSTGRESQL_BRONZE_URL) {
-    //sequelize = new Sequelize(process.env[config.use_env_variable], config);
-    sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_BRONZE_URL, config);
+//
+if (config.use_env_variable) {
+    sequelize = new Sequelize(process.env[config.use_env_variable], config);
+    //sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_BRONZE_URL, config);
 } else {
     sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
